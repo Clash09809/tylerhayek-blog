fa5ef8f667140bec219995b47b5831bbf69b76a2
Clash09809 <clashroyale09809@gmail.com>
2020-06-23 21:24:36 +0530

    Added sitemap

--- src/_data/articles.js
+++ src/_data/articles.js
@@ -8,7 +8,8 @@ module.exports = axios
   .get(`${process.env.HOST}/articles`)
   .then(function (response) {
     var articlesArray = [];
-    response.data.forEach((data) => {
+    response.data.forEach((data) => { 
+
       var artObj = {
         id: data.id,
         title: data.title,
@@ -25,7 +26,9 @@ module.exports = axios
         authorFb: data.author.facebook_profile_link,
         authorInsta: data.author.instagram_profile_link,
         authorTwitter: data.author.twitter_profile_link,
-        authorAvatar: data.author.avatar.url
+        authorAvatar: data.author.avatar.url,
+        created: data.created_at,
+        updated: data.updated_at
       };
       articlesArray.unshift(artObj);
     });
